7f8eec45e733d528ab26a43ef4c37f59da49f5fc
prabhu2304 <prabhu.murlidhar@gmail.com>
2021-03-14 12:04:48 +0530

    Add files via upload

--- Simple Linear Regression/delivery_time.R
+++ Simple Linear Regression/delivery_time.R
@@ -1,15 +1,51 @@
 df <- read.csv("delivery_time.csv")
 View(df)
-library('lattice')
 
 #EDA
 boxplot(df$Sorting.Time)
 boxplot(df$Delivery.Time)
 plot(df$Sorting.Time, df$Delivery.Time, main = "Scatter Plot")
+cor(df$Sorting.Time,df$Delivery.Time)
+summary(df)
+is.na(df)
 
 #Linear Regression Model
 model <- lm(Delivery.Time~Sorting.Time, data=df)
 summary(model)
-
-#Prediction
+model$residuals
 predict(model, data.frame(Sorting.Time=c(10,9,8,7,6,5,4)))
+
+#log transformation
+plot(log(df$Sorting.Time),df$Delivery.Time)
+cor(log(df$Sorting.Time),df$Delivery.Time)
+reg_log <- lm(Delivery.Time~log(Sorting.Time), data=df)
+summary(reg_log)
+reg_log$residuals
+predict(reg_log,data.frame(Sorting.Time=c(10,9,8,7,6,5,4)))
+
+
+#exponential transformation
+reg_exp <- lm(log(Delivery.Time)~Sorting.Time, data=df)
+summary(reg_exp)
+reg_exp$residuals
+predict(reg_exp,data.frame(Sorting.Time=c(10,9,8,7,6,5,4)))
+
+
+#polynomial transformation
+cor(df$Sorting.Time*df$Sorting.Time,df$Delivery.Time)
+reg_poly <- lm(Delivery.Time~Sorting.Time+I(Sorting.Time^2), data=df)
+summary(reg_poly)
+predict(reg_poly)
+reg_poly$residuals
+predict(reg_poly, data.frame(Sorting.Time=c(10,9,8,7,6,5,4)))
+
+
+#cubic transformation
+cor(df$Sorting.Time^3,df$Delivery.Time)
+reg_poly3 <- lm(Delivery.Time~Sorting.Time+I(Sorting.Time^2)+I(Sorting.Time^3), data=df)
+summary(reg_poly3)
+reg_poly3$residuals
+predict(reg_poly3,data.frame(Sorting.Time=c(10,9,8,7,6,5,4)))
+
+#exponential model has higher value of r^2 value
+plot(reg_exp)
--- Simple Linear Regression/salary_prediction.R
+++ Simple Linear Regression/salary_prediction.R
@@ -5,7 +5,47 @@ View(df)
 boxplot(df$YearsExperience)
 boxplot(df$Salary)
 plot(df$YearsExperience, df$Salary, main="Scatter Plot")
+cor(df)
+summary(df)
+is.na(df)
 
 #Linear Regression Model
 model <- lm(Salary~YearsExperience, data=df)
 summary(model)
+model$residuals
+predict(model,data.frame(YearsExperience=c(1.5,2.2,3.3,4.9)))
+
+#log transformation
+plot(log(df$YearsExperience),df$Salary)
+cor(log(df$YearsExperience),df$Salary)
+reg_log <- lm(Salary~log(YearsExperience), data=df)
+summary(reg_log)
+reg_log$residuals
+predict(reg_log,data.frame(YearsExperience=c(1.5,2.2,3.3,4.9)))
+
+#exponential transformation
+plot(df$YearsExperience,log(df$Salary))
+cor(df$YearsExperience,log(df$Salary))
+reg_exp <- lm(log(Salary)~YearsExperience, data=df)
+summary(reg_exp)
+reg_exp$residuals
+predict(reg_exp,data.frame(YearsExperience=c(1.5,2.2,3.3,4.9)))
+
+#polynomial transformation
+plot(df$YearsExperience*df$YearsExperience, df$Salary, main='Scatter Plot')
+cor(df$YearsExperience*df$YearsExperience, df$Salary)
+reg_poly <- lm(Salary~YearsExperience+I(YearsExperience^2), data=df)
+summary(reg_poly)
+reg_poly$residuals
+predict(reg_poly, data.frame(YearsExperience=c(1.5,2.2,3.3,4.9)))
+
+#cubic transformation
+plot((df$YearsExperience^3), df$Salary, main="Scatter Plot")
+cor(df$YearsExperience^3,df$Salary)
+reg_poly3 <- lm(Salary~YearsExperience+I(YearsExperience^2)+I(YearsExperience^3), data=df)
+summary(reg_poly3)
+reg_poly3$residuals
+predict(reg_poly3,data.frame(YearsExperience=c(1.5,2.2,3.3,4.9)))
+
+#cubic model has highest r^2 value 
+plot(reg_poly3)
